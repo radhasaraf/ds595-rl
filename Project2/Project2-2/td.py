fc97165da292f51b28aca0ca79ba3518fbae3df5
Radha <radha@skylarkdrones.com>
2022-03-02 16:29:12 -0500

    feat: Complete project 2-2 (Model-free Control)
    Implemented SARSA and Q-learning methods for Cliff-Walking from
    OpenAI Gym

--- Project2/Project2-2/td.py
+++ Project2/Project2-2/td.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
-import random
 from collections import defaultdict
-#-------------------------------------------------------------------------
-'''
+
+import numpy as np
+
+"""
     Temporal Difference
-    In this problem, you will implememnt an AI player for cliffwalking.
-    The main goal of this problem is to get familar with temporal diference algorithm.
-    You could test the correctness of your code 
+    In this problem, you will implement an AI player for cliff-walking.
+    The main goal of this problem is to get familiar with temporal difference algorithm.
+    You could test the correctness of your code
     by typing 'nosetests -v td_test.py' in the terminal.
-    
     You don't have to follow the comments to write your code. They are provided
     as hints in case you need. 
-'''
-#-------------------------------------------------------------------------
+"""
 
-def epsilon_greedy(Q, state, nA, epsilon = 0.1):
+
+def epsilon_greedy(Q, state, nA, epsilon=0.1):
     """Selects epsilon-greedy action for supplied state.
     
     Parameters:
@@ -38,19 +37,17 @@ def epsilon_greedy(Q, state, nA, epsilon = 0.1):
      Hints:
         You can use the function from project2-1
     """
-    ############################
-    # YOUR IMPLEMENTATION HERE #
 
+    greedy_index = np.argmax(Q[state])
+    probability = np.ones(nA)*epsilon/nA  # exploration
+    probability[greedy_index] += 1 - epsilon  # exploitation
 
+    return np.random.choice(np.arange(len(Q[state])), p=probability)
 
-
-
-    ############################
-    return action
 
 def sarsa(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     """On-policy TD control. Find an optimal epsilon-greedy policy.
-    
+
     Parameters:
     -----------
     env: function
@@ -67,54 +64,42 @@ def sarsa(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     --------
     Q: dict()
         A dictionary  that maps from state -> action-values,
-        where Q[s][a] is the estimated action value corresponding to state s and action a. 
+        where Q[s][a] is the estimated action value corresponding to state s and action a.
     Hints:
     -----
     You could consider decaying epsilon, i.e. epsilon = 0.99*epsilon during each episode.
     """
-    
+
     # a nested dictionary that maps state -> (action -> action-value)
-    # e.g. Q[state] = np.darrary(nA)
-    Q = defaultdict(lambda: np.zeros(env.action_space.n))
-    
-    ############################
-    # YOUR IMPLEMENTATION HERE #
-    
-    # loop n_episodes
+    # e.g. Q[state] = np.array(nA)
+    action_count = env.action_space.n
+    Q = defaultdict(lambda: np.zeros(action_count))
 
-        # define decaying epsilon
+    for i in range(n_episodes):
+        epsilon = 0.99*epsilon  # define decaying epsilon
+        curr_state = env.reset()  # initialize the environment
+        curr_action = epsilon_greedy(Q, curr_state, action_count, epsilon)  # get an action from policy
 
+        done = False
+        while not done:  # loop for each step of episode until terminal state
+            next_state, reward, done, info = env.step(curr_action)  # return new state, reward and done
+            next_action = epsilon_greedy(Q, next_state, action_count, epsilon)  # get next action
 
-        # initialize the environment 
-
-        
-        # get an action from policy
-
-        # loop for each step of episode
-
-            # return a new state, reward and done
-
-            # get next action
-
-            
             # TD update
-            # td_target
+            td_target = reward + gamma*Q[next_state][next_action]  # td_target
+            td_error = td_target - Q[curr_state][curr_action]  # td_error
 
-            # td_error
+            Q[curr_state][curr_action] = Q[curr_state][curr_action] + alpha*td_error  # new Q
 
-            # new Q
+            curr_state = next_state  # update state
+            curr_action = next_action  # update action
 
-            
-            # update state
-
-            # update action
-
-    ############################
     return Q
+
 
 def q_learning(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     """Off-policy TD control. Find an optimal epsilon-greedy policy.
-    
+
     Parameters:
     -----------
     env: function
@@ -131,34 +116,30 @@ def q_learning(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     --------
     Q: dict()
         A dictionary  that maps from state -> action-values,
-        where Q[s][a] is the estimated action value corresponding to state s and action a. 
+        where Q[s][a] is the estimated action value corresponding to state s and action a.
     """
     # a nested dictionary that maps state -> (action -> action-value)
-    # e.g. Q[state] = np.darrary(nA)
-    Q = defaultdict(lambda: np.zeros(env.action_space.n))
-    
-    ############################
-    # YOUR IMPLEMENTATION HERE #
-    
+    # e.g. Q[state] = np.array(nA)
+    action_count = env.action_space.n
+    Q = defaultdict(lambda: np.zeros(action_count))
+
     # loop n_episodes
+    for i in range(n_episodes):
+        epsilon = 0.99*epsilon  # define decaying epsilon
+        curr_state = env.reset()  # initialize the environment
 
-        # initialize the environment 
+        done = False
+        while not done:  # loop for each step of episode until terminal state
+            curr_action = epsilon_greedy(Q, curr_state, action_count, epsilon)  # get an action from policy
+            next_state, reward, done, info = env.step(curr_action)  # return a new state, reward and done
 
-        
-        # loop for each step of episode
-
-            # get an action from policy
-            
-            # return a new state, reward and done
-            
             # TD update
-            # td_target with best Q
+            best_action = np.argmax(Q[next_state])
+            td_target = reward + gamma*Q[next_state][best_action]  # td_target with best Q
+            td_error = td_target - Q[curr_state][curr_action]  # td_error
 
-            # td_error
+            Q[curr_state][curr_action] += alpha*td_error  # new Q
 
-            # new Q
-            
-            # update state
+            curr_state = next_state  # update state
 
-    ############################
     return Q
--- Project2/Project2-2/td_test.py
+++ Project2/Project2-2/td_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import gym
-import numpy as np
 import sys
-from collections import defaultdict
-from collections import Counter
+
+import gym
 
 from td import *
+
 """
     This file includes unit test for td.py
     You could test the correctness of your code by
@@ -14,19 +13,16 @@ from td import *
 """
 
 env = gym.make('CliffWalking-v0')
-#---------------------------------------------------------------
 
 
 def test_python_version():
-    '''------Temporal Difference(50 points in total)------'''
+    """------Temporal Difference(50 points in total)------"""
 
     assert sys.version_info[0] == 3  # require python 3
 
-#---------------------------------------------------------------
-
 
 def test_epsilon_greedy():
-    '''epsilon_greedy (0 point)'''
+    """epsilon_greedy (0 point)"""
     Q = defaultdict(lambda: np.zeros(4))
     Q[5][1] = -1
     Q[5][2] = -1
@@ -40,11 +36,9 @@ def test_epsilon_greedy():
 
     assert np.allclose(1 - np.count_nonzero(actions) / 10000, 0.925, atol=0.02)
 
-#---------------------------------------------------------------
-
 
 def test_sarsa():
-    '''SARSA (25 points)'''
+    """SARSA (25 points)"""
     test_policy = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
                             [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]])
     # note: we do not have cliff state in Q_s if using Sarsa
@@ -55,11 +49,9 @@ def test_sarsa():
     assert np.allclose(policy_q.shape, (4, 12))
     assert np.allclose(policy_q[2:, ], test_policy)
 
-#---------------------------------------------------------------
-
 
 def test_q_learning():
-    '''Q_learning (25 points)'''
+    """Q_learning (25 points)"""
     Q_q = q_learning(env, n_episodes=10000, gamma=1.0, alpha=0.01, epsilon=0.1)
     policy_q = np.array([np.argmax(Q_q[key]) if key in Q_q else -1 for key
                          in np.arange(48)]).reshape((4, 12))
