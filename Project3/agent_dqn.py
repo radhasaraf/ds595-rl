e1e7ee8ab892f8cf6fb24f18a7e43e4d70216569
Radha <radha@skylarkdrones.com>
2022-04-06 03:15:38 -0400

    fix: Try a different approach to remove buffer entries
    The approach introduced in commit 5c33929 to remove sampled
    elements from the buffer resulted in a RuntimeError-'Boolean
    value of Tensor with more than one value is ambiguous', the
    cause of which was not apparent.
    
    It was therefore tried to avoid this error in the first place
    by taking an alternate approach to remove the sampled elements
    from the buffer

--- Project3/agent_dqn.py
+++ Project3/agent_dqn.py
@@ -48,9 +48,16 @@ class ReplayBuffer(object):
 
     def sample(self, batch_size: int) -> List[Transition]:
         """Randomly sample 'batch_size' number of transitions from buffer"""
-        samples = random.sample(self.buffer, batch_size)
-        for elem in samples:
-            self.buffer.remove(elem)
+        # samples = random.sample(self.buffer, batch_size)
+        # for elem in samples:
+        #     self.buffer.remove(elem)
+        # return samples
+
+        samples = []
+        for _ in range(batch_size):
+            index = random.randrange(len(self.buffer))
+            samples.append(self.buffer[index])
+            del self.buffer[index]
         return samples
 
     def __len__(self):
